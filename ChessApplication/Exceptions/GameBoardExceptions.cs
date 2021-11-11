a336e5d192629c87841867c56ff5064896ba2190
Eduardo Mafra <edu.r.mafra@gmail.com>
2021-11-11 11:25:12 -0300

    Criando classes especificas para xadrez e o script ChessPosition para transformar o valor de xadrez para matriz

---
+++ ChessApplication/Exceptions/GameBoardExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessApplication.Exceptions
+{
+    class GameBoardExceptions : ApplicationException
+    {
+        public GameBoardExceptions(string msg) : base(msg)
+        {
+
+        }
+    }
+}
